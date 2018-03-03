0d8e5ca7bd19fcc8c0d91e97bfd8747264e0b5c5
Martin <2777879315@qq.com>
2018-03-03 22:16:40 +0800

    成功编码出块头的RLP数据

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -16,11 +16,14 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
 	"github.com/ethereum/go-ethereum/rlp"
 	//"github.com/ethereum/go-ethereum/core/types"
 
 	"github.com/sammy007/open-ethereum-pool/util"
 )
+
+var bigWordNibbles int
 
 type RPCClient struct {
 	sync.RWMutex
@@ -49,6 +52,7 @@ type GetBlockReply struct {
 type BlockNonce [8]byte
 // MarshalText encodes n as a hex string with 0x prefix.
 func (n BlockNonce) MarshalText() ([]byte, error) {
+log.Printf("MarshalText ^^^^^^^")
 	return hexutil.Bytes(n[:]).MarshalText()
 }
 
@@ -56,6 +60,7 @@ func (n BlockNonce) MarshalText() ([]byte, error) {
 func (n *BlockNonce) UnmarshalText(input []byte) error {
 	return hexutil.UnmarshalFixedText("BlockNonce", input, n[:])
 }
+
 func (n *BlockNonce) String() string {
 	return "0x" + hex.EncodeToString(n[:])
 }
@@ -82,6 +87,119 @@ func (b *BloomType) String() string {
 	return "0x" + hex.EncodeToString(b[:])
 }
 
+type Big big.Int
+const badNibble = ^uint64(0)
+
+func EncodeBig(bigint *big.Int) string {
+	nbits := bigint.BitLen()
+	if nbits == 0 {
+		return "0x0"
+	}
+	return fmt.Sprintf("%#x", bigint)
+}
+
+func (b Big) MarshalText() ([]byte, error) {
+	log.Printf("^^^^^^^")
+	tmpStr := EncodeBig((*big.Int)(&b))
+	ret := []byte(tmpStr)
+	log.Printf("^^^^^^^ MarshalText : " + tmpStr + "  result len: ", len(ret))
+	return ret, nil
+}
+
+func decodeNibble(in byte) uint64 {
+	switch {
+	case in >= '0' && in <= '9':
+		return uint64(in - '0')
+	case in >= 'A' && in <= 'F':
+		return uint64(in - 'A' + 10)
+	case in >= 'a' && in <= 'f':
+		return uint64(in - 'a' + 10)
+	default:
+		return ^uint64(0)
+	}
+}
+
+func bytesHave0xPrefix(input []byte) bool {
+	return len(input) >= 2 && input[0] == '0' && (input[1] == 'x' || input[1] == 'X')
+}
+func checkNumberText(input []byte) (raw []byte, err error) {
+	if len(input) == 0 {
+		return nil, nil // empty strings are allowed
+	}
+	if !bytesHave0xPrefix(input) {
+		return nil, hexutil.ErrMissingPrefix
+	}
+	input = input[2:]
+	if len(input) == 0 {
+		return nil, hexutil.ErrEmptyNumber
+	}
+	if len(input) > 1 && input[0] == '0' {
+		return nil, hexutil.ErrLeadingZero
+	}
+	return input, nil
+}
+// UnmarshalJSON implements json.Unmarshaler.
+func (b *Big) UnmarshalText(input []byte) error {
+	raw, err := checkNumberText(input)
+	if err != nil {
+		return err
+	}
+	if len(raw) > 64 {
+		return hexutil.ErrBig256Range
+	}
+	words := make([]big.Word, len(raw)/bigWordNibbles+1)
+	end := len(raw)
+	for i := range words {
+		start := end - bigWordNibbles
+		if start < 0 {
+			start = 0
+		}
+		for ri := start; ri < end; ri++ {
+			nib := decodeNibble(raw[ri])
+			if nib == badNibble {
+				return hexutil.ErrSyntax
+			}
+			words[i] *= 16
+			words[i] += big.Word(nib)
+		}
+		end = start
+	}
+	var dec big.Int
+	dec.SetBits(words)
+	*b = (Big)(dec)
+	return nil
+}
+
+func (b *Big) String() string {
+	return EncodeBig(b.ToInt())
+}
+
+func (b *Big) ToInt() *big.Int {
+	return (*big.Int)(b)
+}
+
+
+
+type FakeBigInt struct{
+	content string
+}
+
+func (obj FakeBigInt) MarshalText() ([]byte, error){
+	ret, err := hex.DecodeString(obj.content)
+	return ret, err
+}
+
+func (obj *FakeBigInt) UnmarshalText(input []byte) error{
+	obj.content = hex.EncodeToString(input[:])
+	log.Printf("-------------- : " + obj.content)
+	return nil
+}
+
+func (obj *FakeBigInt) String() string{
+	return obj.content
+}
+
+
 type BlockHeader struct {
 	ParentHash  common.Hash    `json:"parentHash"       gencodec:"required"`
 	UncleHash   common.Hash    `json:"sha3Uncles"       gencodec:"required"`
@@ -90,20 +208,51 @@ type BlockHeader struct {
 	TxHash      common.Hash    `json:"transactionsRoot" gencodec:"required"`
 	ReceiptHash common.Hash    `json:"receiptsRoot"     gencodec:"required"`
 	Bloom       BloomType      `json:"logsBloom"        gencodec:"required"`
-	Difficulty  *hexutil.Big   `json:"difficulty"       gencodec:"required"`
-	Number      *hexutil.Big   `json:"number"           gencodec:"required"`
+	Difficulty  hexutil.Uint64 `json:"difficulty"       gencodec:"required"`
+	Number      hexutil.Uint64 `json:"number"           gencodec:"required"`
 	GasLimit    hexutil.Uint64 `json:"gasLimit"         gencodec:"required"`
 	GasUsed     hexutil.Uint64 `json:"gasUsed"          gencodec:"required"`
-	Time        *hexutil.Big   `json:"timestamp"        gencodec:"required"`
+	Time        hexutil.Uint64 `json:"timestamp"        gencodec:"required"`
 	Extra       hexutil.Bytes  `json:"extraData"        gencodec:"required"`
 	MixDigest   common.Hash    `json:"mixHash"          gencodec:"required"`
 	Nonce       BlockNonce     `json:"nonce"            gencodec:"required"`
+}
+
+func (h *BlockHeader) Hash() common.Hash {
+	return rlpHash(h)
+}
+
+// HashNoNonce returns the hash which is used as input for the proof-of-work search.
+func (h *BlockHeader) HashNoNonce() common.Hash {
+	return rlpHash([]interface{}{
+		h.ParentHash,
+		h.UncleHash,
+		h.Coinbase,
+		h.Root,
+		h.TxHash,
+		h.ReceiptHash,
+		h.Bloom,
+		h.Difficulty,
+		h.Number,
+		h.GasLimit,
+		h.GasUsed,
+		h.Time,
+		h.Extra,
+	})
+}
+
+func rlpHash(x interface{}) (h common.Hash) {
+	hw := sha3.NewKeccak256()
+	rlp.Encode(hw, x)
+	hw.Sum(h[:0])
+	return h
 }
 
 type Block struct {
 	Header       *BlockHeader
 	uncles       []*BlockHeader
 }
+
 
 type GetBlockReplyPart struct {
 	Number     string `json:"number"`
@@ -180,6 +329,19 @@ func (r *RPCClient) GetBlockByHeight(height int64) (*GetBlockReply, error) {
 	return r.getBlockBy("eth_getBlockByNumber", params)
 }
 
+func (r *RPCClient) Init() {
+	// This is a weird way to compute the number of nibbles required for big.Word.
+	// The usual way would be to use constant arithmetic but go vet can't handle that.
+	b, _ := new(big.Int).SetString("FFFFFFFFFF", 16)
+	switch len(b.Bits()) {
+	case 1:
+		bigWordNibbles = 16
+	case 2:
+		bigWordNibbles = 8
+	default:
+		panic("weird big.Word size")
+	}
+}
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 func (r *RPCClient) GetBlockRLP(height int64) (string){
@@ -201,6 +363,7 @@ func (r *RPCClient) GetBlockRLP(height int64) (string){
 }
 
 func (r *RPCClient) GetHeaderRLP(height int64){
+	r.Init()
 	params := []interface{}{fmt.Sprintf("0x%x", height), true}
 	method := "eth_getBlockByNumber"
 	
@@ -234,6 +397,22 @@ func (r *RPCClient) GetHeaderRLP(height int64){
 		log.Printf("Extra: "+header.Extra.String())
 		log.Printf("MixDigest: "+header.MixDigest.String())
 		log.Printf("Nonce: "+header.Nonce.String())
+		
+		headerRlpByte, hErr := rlp.EncodeToBytes(header)
+		if(hErr != nil){
+			log.Printf("header EncodeToBytes error: ", hErr.Error())
+			return
+		}
+		
+		log.Printf("headerRlpByte::::::", hex.EncodeToString(headerRlpByte[:]))
+		
+		log.Printf("++++++++++++++++++++++++++")
+		log.Printf("header hash A: ", header.Hash().String())
+		hw := sha3.NewKeccak256()
+		hw.Write(headerRlpByte)
+		var h common.Hash
+		hw.Sum(h[:0])
+		log.Printf("header hash B: ", h.String())
 	}
 }
 
